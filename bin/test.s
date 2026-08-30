24cb02ba484454fa72284382dc7f4fe2e9e155c1
Ameer <ameergamez06@gmail.com>
2026-08-30 05:13:38 -0400

    sync

--- bin/test.s
+++ bin/test.s
@@ -7,6 +7,7 @@
 	SIZE:	.long	10
 	LR4:	.asciz "[Entry @ %p] %c (%d, %d, %d)\n"
 	LR5:	.asciz "sizeof(Entry) is %zu bytes\n"
+	LR6:	.asciz "%d\n"
 
 .section .text
 .global main
@@ -46,43 +47,51 @@ main:
   mov	ebx, 13
   idiv	ebx
   mov	DWORD PTR [rbp - 134], eax
-  mov	DWORD PTR [rbp - 138], 0
+  lea	rax, [rbp - 134]
+  mov	QWORD PTR [rbp - 142], rax
+  mov	rax, QWORD PTR [rbp - 142]
+  mov	QWORD PTR [rax], 20
+  mov	esi, DWORD PTR [rbp - 134]
+  lea	rdi, [rip + LR6]
+  xor	al, al
+  call	printf
+  mov	DWORD PTR [rbp - 146], 0
 L0:
-  mov	ebx, DWORD PTR [rbp - 138]
+  mov	ebx, DWORD PTR [rbp - 146]
   cmp	ebx, DWORD PTR [rbp - 134]
   setl	al
   movzx	eax, al
   test	eax, eax
   je	L1
-  mov	eax, DWORD PTR [rbp - 138]
+  mov	eax, DWORD PTR [rbp - 146]
   imul	eax, 13
   movsx	rbx, eax
   lea	rax, [rbp - 130]
   add	rax, rbx
-  mov	QWORD PTR [rbp - 146], rax
-  mov	rax, QWORD PTR [rbp - 146]
+  mov	QWORD PTR [rbp - 154], rax
+  mov	rax, QWORD PTR [rbp - 154]
   mov	BYTE PTR [rax + 0], 84
-  mov	rax, QWORD PTR [rbp - 146]
-  mov	ebx, DWORD PTR [rbp - 138]
+  mov	rax, QWORD PTR [rbp - 154]
+  mov	ebx, DWORD PTR [rbp - 146]
   mov	DWORD PTR [rax + 1], ebx
-  mov	rax, QWORD PTR [rbp - 146]
+  mov	rax, QWORD PTR [rbp - 154]
   mov	DWORD PTR [rax + 5], 5
-  mov	rax, QWORD PTR [rbp - 146]
+  mov	rax, QWORD PTR [rbp - 154]
   mov	DWORD PTR [rax + 9], 6
 L2:
-  mov	eax, DWORD PTR [rbp - 138]
-  add	DWORD PTR [rbp - 138], 1
+  mov	eax, DWORD PTR [rbp - 146]
+  add	DWORD PTR [rbp - 146], 1
   jmp	L0
 L1:
-  mov	DWORD PTR [rbp - 142], 0
+  mov	DWORD PTR [rbp - 150], 0
 L3:
-  mov	ebx, DWORD PTR [rbp - 142]
+  mov	ebx, DWORD PTR [rbp - 150]
   cmp	ebx, DWORD PTR [rbp - 134]
   setl	al
   movzx	eax, al
   test	eax, eax
   je	L4
-  mov	eax, DWORD PTR [rbp - 142]
+  mov	eax, DWORD PTR [rbp - 150]
   imul	eax, 13
   movsx	rbx, eax
   lea	rax, [rbp - 130]
@@ -90,8 +99,8 @@ L3:
   mov	rdi, rax
   call	print_entry
 L5:
-  mov	eax, DWORD PTR [rbp - 142]
-  add	DWORD PTR [rbp - 142], 1
+  mov	eax, DWORD PTR [rbp - 150]
+  add	DWORD PTR [rbp - 150], 1
   jmp	L3
 L4:
   xor	eax, eax
